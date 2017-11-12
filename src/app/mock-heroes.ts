ef1f4b4b094217d8a7a9db796cfce043486a5bdc
garg10may <garg10may@gmail.com>
2017-11-12 16:47:25 +0530

    final working app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice'},
+	{ id: 12, name: 'Mr. Narco'},
+	{ id: 13, name: 'Mr. Kolibaba'},
+	{ id: 14, name: 'Mr. ghitorni'},
+	{ id: 15, name: 'Mr. bombasto'},
+	{ id: 16, name: 'Mr. twirla'},
+	{ id: 17, name: 'Mr. Pikoti'},
+	{ id: 18, name: 'Mr. Machila'},
+	{ id: 19, name: 'Mr. Jhingosi'},
+	{ id: 20, name: 'Mr. tumasa'},
+	{ id: 21, name: 'Mr. Chilapi'},
+	{ id: 22, name: 'Mr. kokoniko'}
+	];
